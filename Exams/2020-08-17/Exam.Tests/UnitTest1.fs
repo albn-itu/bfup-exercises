c3d054acaa00a0f10bfae07a651d13c70f40cc46
albe2669 <albert@risenielsen.dk>
2022-06-02 15:17:06 +0200

    Finished 2020-08-17

--- Exams/2020-08-17/Exam.Tests/UnitTest1.fs
+++ Exams/2020-08-17/Exam.Tests/UnitTest1.fs
@@ -1,11 +1,3 @@
 module Exam.Tests
 
 open NUnit.Framework
-
-[<SetUp>]
-let Setup () =
-    ()
-
-[<Test>]
-let Test1 () =
-    Assert.Pass()
---
+++ Exams/2020-08-17/README.md
@@ -0,0 +1,3 @@
+Action      Time    Elapsed
+Started     - 11:20 (0:00)
+1 & 2 done  - 12:50 ()
